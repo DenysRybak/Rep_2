4fd587bb0e401a9c014d780e6adf106f587bd322
DenysRybak <119297864+DenysRybak@users.noreply.github.com>
2022-11-30 16:20:11 +0200

    adding my lab5 write equaliation

---
+++ Home.h
@@ -0,0 +1,15 @@
+
+#include "Pets.h"
+
+class Home {
+private:
+    Pets* group;
+public:
+    Home(Pets group[3]);
+    Pets& getPetByIndex(int i);
+    void printAllPets();
+    void findFriends();
+    int isPolite();
+    const char *greeting;
+    int sortedPets();
+};
