3f7bc1b1f1a24382690a07c668e0db7b51074c2f
DenysRybak <119297864+DenysRybak@users.noreply.github.com>
2022-12-22 11:28:23 +0200

    adding main.cpp update

--- main.cpp
+++ main.cpp
@@ -14,17 +14,17 @@ int main() {
     Pets Dog("Dog Boel","Bulldog", "Bye", 5, 56);
     Pets Cat("Cat Tom","Egypt", "Hello", 3, 21);
     Pets Bird("Parrot Kabes","Wavy", "Hello", 4, 6);
-    Pets groupInhome[3] = {Dog, Cat, Bird};
-    Home den (groupInhome);
+    Pets group_in_home[3] = {Dog, Cat, Bird};
+    Home owner_Daniel (group_in_home);
 
-    den.printAllPets();
+    owner_Daniel .printAllPets();
     std::cout<<"------------------------\n";
-    den.findFriends();
+    owner_Daniel .findFriends();
     std::cout<<"------------------------\n";
-    den.isPolite();
+    owner_Daniel .isPolite();
     std::cout<<"------------------------\n";
-    den.sortedPets();
+    owner_Daniel .sortedPets();
 
-
+    std::cout<<"Goodbye user"<<std:: endl;
     return 0;
 }
