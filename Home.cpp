f4403324e6085b59ee6480c80ae76765884a09e4
DenysRybak <119297864+DenysRybak@users.noreply.github.com>
2022-12-22 11:22:26 +0200

    adding Home.cpp

--- Home.cpp
+++ Home.cpp
@@ -19,15 +19,15 @@ void Home::findFriends(){
             if(i==j){
                 continue;
             }
-            if(this ->getPetByIndex(i).getAge()- this->getPetByIndex(j).getAge()<=1){
-                std::cout<<getPetByIndex(i).getName()<<"and"<<getPetByIndex(j).getName()<<"are friend!\n";
+            if(this ->getPetByIndex(i).get_age()- this->getPetByIndex(j).get_age()<=1){
+                std::cout<<getPetByIndex(i).get_name()<<"and"<<getPetByIndex(j).get_name()<<"are friend!\n";
             }
         }
     }
 }
 int Home :: isPolite() {
     for (int i = 0; i < 3; i++) {
-        if (this ->getPetByIndex(i).getGreeting() == "Hello") {
+        if (this ->getPetByIndex(i).get_greeting() == "Hello") {
             std::cout << "True" << std::endl;
         } else {
             std::cout << "False" << std::endl;
@@ -47,12 +47,14 @@ int Home::sortedPets() {
     Pets* sortedPets = this->group;
     for(int i=0; i<3; i++){
         for (int j=0; j<3-i-1; j++) {
-            if (this->getPetByIndex(j).getAge() < this->getPetByIndex(j + 1).getAge())
+            if (this->getPetByIndex(j).get_age() < this->getPetByIndex(j + 1).get_age())
                 swap(getPetByIndex(j), getPetByIndex(j + 1));
         }
     }
     for (int i=0; i<3; i++) {
       sortedPets[i].printPet();
     }
+
    return 0;
+    //For pets
 }
